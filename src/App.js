3a34fbdab5d2fd4d23f609c276d4b470f5fae497
Nitin Momaya <nitinmomaya5@gmail.com>
2023-01-06 16:12:08 +0530

    React-uUseEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import UseState from './Hooks/UseState';
+// import UseState from './Hooks/UseState';
+import UseEffect from './Hooks/UseEffect';
 
 
 
@@ -9,7 +10,8 @@ function App () {
   return <>
 
     <div className="wrapper">
-      <UseState/>
+      {/* <UseState/> */}
+      <UseEffect/>
     </div>
     
   </>;
